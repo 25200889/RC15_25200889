72c2e835c1009a83fc42d9b18809834a3eaf39c0
25200889 <ucbv226@ucl.ac.uk>
2026-03-29 10:28:56 +0800

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- Skills Module 1 — Cartographies of Affect/Skill 1-p4-Multi‑Dimensional Analysis of Chinese Restaurants-Code.py
+++ Skills Module 1 — Cartographies of Affect/Skill 1-p4-Multi‑Dimensional Analysis of Chinese Restaurants-Code.py
@@ -24,7 +24,7 @@ from collections import Counter
 from scipy.spatial import cKDTree
 
 # ========== Configuration Parameters ==========
-API_KEY = "7e3a26760a42c3a6bc9b66b327df0fc6"   # Your Flickr API Key
+API_KEY = "Flickr_API_Key"   # Your Flickr API Key
 CHINATOWN_BBOX = "-0.1350,51.5085,-0.1250,51.5140"   # London Chinatown bounding box
 
 # Food‑related keywords (for local text classification)
